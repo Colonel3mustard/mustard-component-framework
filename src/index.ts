f1f158e61b0ce4d733617309557c7ffea942deea
Mark Mustard <mark.mustard.dev@gmail.com>
2024-05-30 10:52:22 -0500

    fixes for building

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import MustardBtn from './components/MustardBtn.vue';
+import MustardCard from './components/MustardCard.vue';
+import MustardCheck from './components/MustardCheck.vue';
+import MustardCheckGroup from './components/MustardCheckGroup.vue';
+import MustardDdn from './components/MustardDdn.vue';
+import MustardInput from './components/MustardInput.vue';
+import MustardNavBar from './components/MustardNavBar.vue';
+
+export {
+  MustardBtn,
+  MustardCard,
+  MustardCheck,
+  MustardCheckGroup,
+  MustardDdn,
+  MustardInput,
+  MustardNavBar,
+};
